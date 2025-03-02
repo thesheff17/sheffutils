92d55186dac6596cde60b68c6e6ba3a1a76b8b59
Dan Sheffner <Dan@Sheffner.com>
2025-03-02 11:57:55 -0600

    changed to class, added a test for the timestamp def.

--- test_sheffutils.py
+++ test_sheffutils.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+# standard library imports
+import re
+import unittest
+
+# 3rd party imports
 import sheffutils
 
-def test_hello_world():
-    assert "hello world" == sheffutils.hello_world()
+class TestDateFormat(unittest.TestCase):
+    def test_hello_world(self):
+        assert "hello world" == sheffutils.hello_world()
+
+    def test_date_format(self):
+        """Tests the format YYYY-MM-DD-HH-MM-SS."""
+        pattern = r"^\d{4}-\d{2}-\d{2}-\d{2}-\d{2}-\d{2}$"
+
+        # Positive test cases
+        self.assertTrue(re.match(pattern, "2025-03-02-11-01-50"))
